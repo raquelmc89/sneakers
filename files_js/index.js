3e0960d56fb42a570a1b271203ff83813b6fa026
Raquel MC <raquelmc89@gmail.com>
2022-10-04 23:37:33 +0100

    cart working css needed

--- files_js/index.js
+++ files_js/index.js
@@ -10,7 +10,7 @@ const addToCart = document.querySelector('#addToCart');
 const cartIcon = document.getElementById('cartIcon');
 const cartEmpty = document.querySelector('.cart-Empty');
 const cartFull = document.querySelector('.cart-full');
-
+const cartIconNumber = document.getElementById("cartIconNumber");
 
 
 
@@ -71,70 +71,66 @@ minusButtonClicked
 addToCart.addEventListener('click', addToCartClicked);
 
 function addToCartClicked(event){
- const buttonClicked = event.target;
-console.log(quantityInput.value)
+    const buttonClicked = event.target;
+    cartIconNumber.innerText = quantityInput.value;
 
 }
 addToCartClicked;
 
-/*
-    cartIcon.addEventListener("click", cartIconClicked)
-   
-     function cartIconClicked(event){
-    buttonCart = event.target;
-    console.log("hola");
-     cartEmpty.style.display = "block";
+
+cartIcon.addEventListener("click", cartIconClicked)
+
+function cartIconClicked(event){
+    const buttonClicked = event.target
+
+    if(cartIconNumber.innerText == 0)
+    {
+        $(document).ready(function(){
+
+            $("#cartIcon").click(function(){
+              $(".cart-Empty").toggle();
+            });
+            
+            });
+     } else{
+        shoppingCartRow.innerHTML = shoppingCartContent;
+        cartFull.append(shoppingCartRow);
      }
+     
+    }
+    
+  //CART FULL DIV
 
-*/
-// CART EMPTY
-$(document).ready(function(){
-
-
-$("#cartIcon").click(function(){
-  console.log("ra")
-  $(".cart-Empty").toggle();
-});
-
-});
-
-//CART FULL
-
-
-const shoppingCartRow = document.createElement('div');
-const shoppingCartContent =
-
- `<div class="row shoppingCartItem" data-id=${sneakers.name}>
-<div class="col-6">
-    <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-        <img src="./images/image-product-1-thumbnail.jpg" class="shopping-cart-image">
-        <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${sneakers.name}</h6>
+    const shoppingCartRow = document.createElement('div');
+    const shoppingCartContent =
+     `<div class="row shoppingCartItem" data-id=${sneakers.name}>
+    <div class="col-6">
+        <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+            <img src="./images/image-product-1-thumbnail.jpg" class="shopping-cart-image">
+            <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${sneakers.name}</h6>
+        </div>
     </div>
-</div>
-<div class="col-2">
-    <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-        <p class="item-price mb-0 shoppingCartItemPrice">${sneakers.prize}</p>
+    <div class="col-2">
+        <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+            <p class="item-price mb-0 shoppingCartItemPrice">${sneakers.prize}</p>
+        </div>
+        <div class="shopping-cart-amount d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+            <p class="item-amount mb-0 shoppingCartItemAmount">${sneakers.amount}</p>
+        </div>
     </div>
-    <div class="shopping-cart-amount d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-        <p class="item-amount mb-0 shoppingCartItemAmount">${sneakers.amount}</p>
+    <div class="col-4">
+        <div>
+            <button class= "delete" type="button"><img src="./images/icon-delete.svg" class="shopping-cart-image"></button>
+        </div>
     </div>
-</div>
-<div class="col-4">
-    <div>
-        <button class= "delete" type="button"><img src="./images/icon-delete.svg" class="shopping-cart-image"></button>
-    </div>
-</div>
-     <div> <button id="addToCart">Checkout</button>
-</div>`;
-console.log("🚀 ~ file: index.js ~ line 121 ~ shoppingCartContent ", shoppingCartContent )
+         <div> <button id="addToCart">Checkout</button>
+    </div>`;
+    
+   
+    
 
-shoppingCartRow.innerHTML = shoppingCartContent;
-cartFull.append(shoppingCartRow);
-
-
-
-
-
+    
+    
 
 
 
